d0e4eb54551509258e44f0fddce863c02090f500
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-08-18 22:23:46 -0400

    edit to scrape

--- client2/src/components/StartScrape.tsx
+++ client2/src/components/StartScrape.tsx
@@ -1,116 +1,137 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm} from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form"
-import {Select,SelectContent,SelectItem,SelectTrigger,SelectValue,} from "@/components/ui/select"
-import { toast } from "@/components/ui/use-toast"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "@/components/ui/use-toast";
+import { Input } from "@/components/ui/input";
 
-import axios from 'axios';
+import axios from "axios";
 
 const formSchema = z.object({
-    search_term: z.string().min(2, {
-      message: "Search term must be at least 2 characters.",
-    }),
-    depth: z.number({
-      required_error: "Please select a depth.",
-    })
-})
+  search_term: z.string().min(2, {
+    message: "Search term must be at least 2 characters.",
+  }),
+  depth: z.number({
+    required_error: "Please select a depth.",
+  }),
+});
 
 export function StartScrape() {
-    const defaultValues = {
-        search_term: "",
-        depth: 2
+  const defaultValues = {
+    search_term: "",
+    depth: 2,
+  };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
+    form.reset(defaultValues);
+
+    try {
+      const response = await axios.post("/api/start-scraping", values);
+      toast({
+        title: "Success!",
+        description: (
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            {response.data.message}
+          </p>
+        ),
+      });
+    } catch (error) {
+      toast({
+        title: "Something went wrong!",
+      });
     }
-
-    const form = useForm<z.infer<typeof formSchema>>({// 1. Define your form.
-      resolver: zodResolver(formSchema),
-      defaultValues,
-    })
-    
-    async function onSubmit(values: z.infer<typeof formSchema>) {// 2. Define a submit handler.
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-                <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-                </pre>
-            ),
-        })
-        form.reset(defaultValues)
-  
-        try {
-            const response = await axios.post('/api/start-scraping', values); 
-            toast({
-              title: "Success!",
-              description: (
-                  <p className="leading-7 [&:not(:first-child)]:mt-6">{response.data.message}</p>
-              ),
-            })
-          } catch (error) {
-            toast({
-              title: "Something went wrong!",
-            })
-          }
-        }
-
-    return (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Start Scraping Process</h3>
-            <FormField
-              control={form.control}
-              name="search_term"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Search Term</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Hunter Biden" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Please provide a relevant search term.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-                control={form.control}
-                name="depth"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Max Scraping Depth</FormLabel>
-                    <Select 
-                        onValueChange={(value : string) => field.onChange(Number(value))} 
-                        defaultValue={field.value !== undefined ? field.value.toString() : "2"}
-                        value={field.value?.toString() || "2"}
-                    >
-                        <FormControl>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Please select a max depth to scrape." />
-                        </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                            <SelectItem value="2">{2}</SelectItem>
-                            <SelectItem value="3">{3}</SelectItem>
-                            <SelectItem value="4">{4}</SelectItem>
-                            <SelectItem value="5">{5}</SelectItem>
-                            <SelectItem value="6">{6}</SelectItem>
-                        </SelectContent>
-                    </Select>
-                    <FormDescription>
-                        Be aware that the more depth you go the longer it will take to scrape.
-                    </FormDescription>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      )
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Start Scraping Process
+        </h3>
+        <FormField
+          control={form.control}
+          name="search_term"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Search Term</FormLabel>
+              <FormControl>
+                <Input placeholder="Hunter Biden" {...field} />
+              </FormControl>
+              <FormDescription>
+                Please provide a relevant search term.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="depth"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Max Scraping Depth</FormLabel>
+              <Select
+                onValueChange={(value: string) => field.onChange(Number(value))}
+                defaultValue={
+                  field.value !== undefined ? field.value.toString() : "2"
+                }
+                value={field.value?.toString() || "2"}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Please select a max depth to scrape." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="2">{2}</SelectItem>
+                  <SelectItem value="3">{3}</SelectItem>
+                  <SelectItem value="4">{4}</SelectItem>
+                  <SelectItem value="5">{5}</SelectItem>
+                  <SelectItem value="6">{6}</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Be aware that the more depth you go the longer it will take to
+                scrape.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
